6c429f900da6a6dd89e44436a5bf63178fe6b11c
BPower0036 <80090789+BPower0036@users.noreply.github.com>
2022-06-01 05:05:07 +0000

    Update user.js

--- FireFox/user.js
+++ FireFox/user.js
@@ -18,12 +18,15 @@ user_pref("layout.css.backdrop-filter.enabled", true);
 user_pref("browser.compactmode.show", true);
 
 // ** Theme Custom Options ************************************************************
+user_pref("userChrome+Content.additions.enable_selection", false);
 user_pref("userChrome+Content.additions.private_mode", true);
 user_pref("userChrome+Content.additions.uBlock_Origin.hide_default_unused", true);
 user_pref("userChrome+Content.additions.uBlock_Origin.show_shortcuts", false);
+user_pref("userChrome+Content.padding.bookmark_menu.compact", false);
 user_pref("userChrome+Content.padding.drag_space", true);
 user_pref("userChrome+Content.padding.first_tab", true);
 user_pref("userChrome+Content.padding.megabar_style", true);
+user_pref("userChrome+Content.padding.menu_compact", false);
 user_pref("userChrome+Content.page.field_border", false);
 user_pref("userChrome+Content.page.proton_color.system_accent", true);
 user_pref("userChrome+Content.player", true);
@@ -32,8 +35,9 @@ user_pref("userChrome+Content.tab.close_button_at_hover_pinned", true);
 user_pref("userChrome+Content.tab.close_button_at_hover_pinned.always", true);
 user_pref("userChrome+Content.tab.show_(un)muted_icon_pinned", false);
 user_pref("userChrome+Content.tab.show_sound_label", false);
-user_pref("userChrome+Content.urlview.permissions.hide_granded-icon", true);
+user_pref("userChrome+Content.urlview.permissions.hide_granded-icon", false);
 user_pref("userChrome+Content.zavety", false);
+
 
 // ** Useful Options ******************************************************************
 // Integrated calculator at urlbar
