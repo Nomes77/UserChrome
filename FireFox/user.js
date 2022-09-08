d24000d058f8545649e35b24f169068208bdb9aa
BPower0036 <80090789+BPower0036@users.noreply.github.com>
2022-09-08 11:46:30 +0000

    Update user.js

--- FireFox/user.js
+++ FireFox/user.js
@@ -18,10 +18,12 @@ user_pref("layout.css.backdrop-filter.enabled", true);
 user_pref("browser.compactmode.show", true);
 
 // ** Theme Custom Options ************************************************************
-user_pref("userChrome+Content.additions.enable_selection", true);          // enable selection and copying on every website
+user_pref("userChrome+Content.additions.enable_selection", false);          // enable selection and copying on every website
 user_pref("userChrome+Content.additions.private_mode", true);
 user_pref("userChrome+Content.additions.uBlock_Origin.hide_default_unused", true);
 user_pref("userChrome+Content.additions.uBlock_Origin.show_shortcuts", false);
+user_pref("userChrome+Content.autohide.forward_button", false);
+user_pref("userChrome+Content.compatibility.accent_color", true);
 user_pref("userChrome+Content.padding.bookmark_menu.compact", false);
 user_pref("userChrome+Content.padding.drag_space", true);
 user_pref("userChrome+Content.padding.first_tab", true);
@@ -29,12 +31,18 @@ user_pref("userChrome+Content.padding.megabar_style", true);
 user_pref("userChrome+Content.padding.menu_compact", false);
 user_pref("userChrome+Content.page.field_border", false);
 user_pref("userChrome+Content.page.proton_color.system_accent", true);
+user_pref("userChrome+Content.page.proton_color.system_accent.theme1", false);
+user_pref("userChrome+Content.page.proton_color.system_accent.theme2+5", false);
+user_pref("userChrome+Content.page.proton_color.system_accent.theme3", false);
+user_pref("userChrome+Content.page.proton_color.system_accent.theme4", false);
+user_pref("userChrome+Content.page.proton_color.system_accent.theme6+7", true);
 user_pref("userChrome+Content.player", true);
 user_pref("userChrome+Content.rounding.square", false);
 user_pref("userChrome+Content.tab.close_button_at_hover_pinned", true);
 user_pref("userChrome+Content.tab.close_button_at_hover_pinned.always", true);
 user_pref("userChrome+Content.tab.show_(un)muted_icon_pinned", false);
 user_pref("userChrome+Content.tab.show_sound_label", false);
+user_pref("userChrome+Content.tab.vertical_tab", false);
 user_pref("userChrome+Content.urlview.permissions.hide_granded-icon", false);
 user_pref("userChrome+Content.zavety", false);
 
