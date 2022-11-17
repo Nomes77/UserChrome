4cf73634f654c5fda195ebbe3ee24c8fba83acb2
BPower0036 <80090789+BPower0036@users.noreply.github.com>
2022-11-17 16:20:12 +0000

    `userChrome+Content.urlview.permissions.hide_granded-icon` set to `true` because problem is solved

--- FireFox/user.js
+++ FireFox/user.js
@@ -53,7 +53,7 @@ user_pref("userChrome+Content.tab.vertical_tab", false);
 user_pref("userChrome+Content.theme.coole_breeze", false);
 user_pref("userChrome+Content.theme.private_mode", true);
 user_pref("userChrome+Content.tor.compatibility", false);
-user_pref("userChrome+Content.urlview.permissions.hide_granded-icon", false);
+user_pref("userChrome+Content.urlview.permissions.hide_granded-icon", true);
 user_pref("userChrome+Content.zavety", false);
 
 // ** Useful Options ******************************************************************
